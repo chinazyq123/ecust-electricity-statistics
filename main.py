aa1add08e110c1a81aa022c67ab87155f057e06a
chinazyq123 <32483825+chinazyq123@users.noreply.github.com>
2023-05-26 09:35:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from contextlib import suppress
 from pathlib import Path
-import json,datetime,os,requests,re
+import json
+import datetime
+import os
+import requests
+import re
+import smtplib
+from email.mime.text import MIMEText
 
-# from playwright.sync_api import sync_playwright
-# with sync_playwright() as playwright:
-#     browser = playwright.chromium.launch(headless=True)
-#     context = browser.new_context(
-#         user_agent='Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; Chitanda/Akari) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 MicroMessenger/6.0.0.58_r884092.501 NetType/WIFI'
-#     )
-#     page = context.new_page()
-#     url = os.environ.get('URL').strip()
-#     page.goto(url)
-#     remain = float(page.locator('text=/\d+\.\d+度/i').inner_text().rstrip("度"))
-
+# Retrieve data from the specified URL
 url = os.environ.get('URL').strip()
 header = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -26,23 +22,49 @@ header = {
     'Sec-Fetch-User': '?1',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; Chitanda/Akari) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 MicroMessenger/6.0.0.58_r884092.501 NetType/WIFI'
+    # ... headers ...
 }
 response = requests.get(url, headers=header)
+remain = float(re.findall(r"(\d+(\.\d+)?)度", response.text)[0][0])
 
-remain = float(re.findall((r"(\d+(\.\d+)?)度"),response.text)[0][0])
+# Update data in the JSON file
 originstring = '[]'
 data = []
 date = datetime.datetime.now().strftime("%Y-%m-%d")
 
 with suppress(FileNotFoundError):
-    with open("data.js",'r',encoding='utf-8') as f:
+    with open("data.js", 'r', encoding='utf-8') as f:
         originstring = f.read().lstrip("data=")
 
 data = json.loads(originstring)
 if len(data) != 0 and date in data[-1].values():
     data[-1]['kWh'] = remain
 else:
-    data.append({"time" : date, "kWh" : remain})
+    data.append({"time": date, "kWh": remain})
 
-originstring = json.dumps(data,indent=4, ensure_ascii=False)
+originstring = json.dumps(data, indent=4, ensure_ascii=False)
 Path("data.js").write_text("data=" + originstring)
+
+# Send email if remaining electricity is below 5
+if remain < 50:
+    sender_email = "your_email@example.com"
+    receiver_email = ["chinazyq123@139.com", "18210382197@129.com"]
+    subject = "低电量提醒"
+    message = f"剩余电量不足5度，请及时充电: {remain} kWh."
+
+    # Create a plain text email message
+    email = MIMEText(message)
+    email["Subject"] = subject
+    email["From"] = sender_email
+    email["To"] = receiver_email
+
+    # Send the email
+    smtp_server = "smtp.example.com"
+    smtp_port = 587
+    smtp_username = "your_username"
+    smtp_password = "your_password"
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.send_message(email)
